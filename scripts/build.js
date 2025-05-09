7b7eb3dab3a44d8f5b6403b9dc05342f4bc6adb7
Stephen <hellostephenwm@gmail.com>
2025-05-09 17:49:27 -0400

    chore(build): refactor script to delegate cleanup to `clean` script

--- scripts/build.js
+++ scripts/build.js
@@ -2,22 +2,6 @@
 
 import { execSync } from 'node:child_process';
 import console from 'node:console';
-import { existsSync, rmSync } from 'node:fs';
-
-const distPath = './dist';
-const tsbuildinfo = './.tsbuildinfo';
-
-// Delete dist folder if it exists
-if (existsSync(distPath)) {
-	console.log("🗑️ Deleting existing 'dist/' folder...");
-	rmSync(distPath, { recursive: true, force: true });
-}
-
-// Delete '.tsbuildinfo' file if it exists
-if (existsSync(tsbuildinfo)) {
-	console.log("🗑️ Deleting existing '.tsbuildinfo' file...");
-	rmSync(tsbuildinfo, { recursive: true, force: true });
-}
 
 // Run TypeScript compiler
 console.log('🏗️ Building project...');
