633ec5df5571e7fe528eeeff63a5901611fe410a
Stephen <hellostephenwm@gmail.com>
2025-05-10 00:15:01 -0400

    chore(eslint): disable unicorn/no-null rule

--- src/libs/cors.ts
+++ src/libs/cors.ts
@@ -9,6 +9,7 @@ const whitelist = ['http://localhost:8080'];
 export const corsOptions: CorsOptions = {
 	origin: (origin, callback) => {
 		if (!origin || whitelist.includes(origin)) {
+			// eslint-disable-next-line unicorn/no-null
 			callback(null, true);
 		} else {
 			callback(new Error(`Origin '${origin}' not allowed by CORS`));
