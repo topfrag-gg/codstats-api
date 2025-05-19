5b423bc974a5b2a81222be7059c0c2b1fe079361
Stephen <hellostephenwm@gmail.com>
2025-05-19 00:45:33 -0400

    fix(server): replace 'console.log' with custom logger for better output and file logging

--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,49 @@
 /** @format */
 
-import console from 'node:console';
 import { exit } from 'node:process';
 import config from 'config';
-import { createApp } from '@/src/app';
+import { createApp } from './app';
+import { logger } from './utils/logger';
 
 export const startServer = (): void => {
 	// Load the server configuration variables
 	const protocol: string = config.get('protocol');
+	const env: string = config.get('env') || 'development';
 	const host: string = process.env.HOST ?? 'localhost';
 	const port: number = Number(process.env.PORT) || 3000;
-	const env: string = process.env.NODE_ENV || 'development';
 
 	const app = createApp();
 	const server = app.listen(port, host);
 
 	server.on('listening', () => {
-		console.log(`Starting server in ${env} mode`);
-		console.log(`Server is listening at: ${protocol}://${host}:${port}`);
+		logger.info(`Starting server in ${env} mode`);
+		logger.info(`Server is listening at: ${protocol}://${host}:${port}`);
 
-		if (env === 'development') console.info('Press CTRL+C to stop the server');
+		if (env === 'development') logger.warn('Press CTRL+C to stop the server');
 	});
 
 	server.on('error', (err: NodeJS.ErrnoException) => {
-		console.error('Server encountered an error:', err);
+		logger.error('Server encountered an error:', err);
 
 		if (err.code === 'EADDRINUSE') {
-			console.error(`Port ${port} is already in use`);
-			console.error('1. Terminate the conflicting process, or');
-			console.error('2. Change the port in the server configuration');
+			logger.error(`Port ${port} is already in use`);
+			logger.error('1. Terminate the conflicting process, or');
+			logger.error('2. Change the port in the server configuration');
 		}
 
 		exit(1);
 	});
 
 	server.on('close', () => {
-		console.log('Cleaning up resources...');
-		console.log('Closing remaining connections...');
+		logger.info('Cleaning up resources...');
+		logger.info('Closing remaining connections...');
 	});
 
 	const gracefulShutdown = (signal: string): void => {
-		console.warn(`${signal} signal received`);
+		logger.warn(`${signal} signal received`);
 
 		server.close(() => {
-			console.log('Server closed gracefully');
+			logger.info('Server closed gracefully');
 			exit(0);
 		});
 	};
@@ -52,12 +52,12 @@ export const startServer = (): void => {
 	process.on('SIGINT', () => gracefulShutdown('SIGINT'));
 
 	process.on('uncaughtException', (err) => {
-		console.error('Uncaught exception:', err);
+		logger.error('Uncaught exception:', err);
 		exit(1);
 	});
 
 	process.on('unhandledRejection', (reason) => {
-		console.error('Unhandled rejection:', reason);
+		logger.error('Unhandled rejection:', reason);
 		exit(1);
 	});
 };
