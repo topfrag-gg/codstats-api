cf251187457acf3f0f67233b86697b8e257f1991
Stephen <hellostephenwm@gmail.com>
2025-05-31 20:32:34 -0400

    chore(script): add pre-build cleanup of stale artifacts to script

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,18 @@
 import { execSync } from 'node:child_process';
 import console from 'node:console';
+import { existsSync, rmSync } from 'node:fs';
+
+// Delete .tsbuildinfo
+if (existsSync('.tsbuildinfo')) {
+	console.log('🚮 Removing stale .tsbuildinfo file...');
+	rmSync('.tsbuildinfo');
+}
+
+// Delete existing dist folder
+if (existsSync('dist')) {
+	console.log('🚮 Deleting existing dist folder...');
+	rmSync('dist', { recursive: true, force: true });
+}
 
 // Run TypeScript compiler
 console.log('🏗️ Building project...');
